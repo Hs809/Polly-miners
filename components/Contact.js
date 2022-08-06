799734ff0dde45d941e7c92148dd4a7212f9e9e4
hitesh <hiteshpal.8097@gmail.com>
2022-08-06 23:28:23 +0530

    changed color update state in contact added notify

--- components/Contact.js
+++ components/Contact.js
@@ -6,6 +6,10 @@ const PUBLIC_KEY = "uMnA-Oe16rc5RzAwE";
 const SERVICE_ID = "service_w5s81km";
 const TEMPLATE_ID = "template_8cee97h";
 
+import Notify from 'simple-notify'
+import 'simple-notify/dist/simple-notify.min.css'
+
+
 const options = [
   { value: "mining rig", label: "Mining Rigs" },
   { value: "mining service", label: "Mining Service" },
@@ -15,20 +19,74 @@ const options = [
 
 function Contact() {
   const [selectedOption, setSelectedOption] = useState(null);
+  const [state, setState] = useState({
+    from_name: "",
+    email: "",
+    mobile: "",
+    message: ""
+  })
 
+  const handleChange = (e) => {
+    const {name,value} = e.target;
+    setState({
+      ...state,
+      [name]:value
+    })
+  }
   const form = useRef();
 
   const sendEmail = (e) => {
     e.preventDefault();
-
+    
     emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form.current, PUBLIC_KEY).then(
       (result) => {
-        console.log(result.text);
+        new Notify({
+          status: 'success',
+          title: 'Success',
+          text: 'We got your message, We will right back at you',
+          effect: 'slide',
+          speed: 200,
+          customClass: null,
+          customIcon: null,
+          showIcon: true,
+          showCloseButton: true,
+          autoclose: false,
+          autotimeout: 3000,
+          gap: 20,
+          distance: 20,
+          type: 1,
+          position: 'right top'
+        })
+        console.log(result.text, "me");
       },
       (error) => {
+        Notify({
+          status: 'error',
+          title: 'Error',
+          text: 'Something might have broke down. You can come back later',
+          effect: 'fade',
+          speed: 300,
+          customClass: null,
+          customIcon: null,
+          showIcon: true,
+          showCloseButton: true,
+          autoclose: false,
+          autotimeout: 3000,
+          gap: 20,
+          distance: 20,
+          type: 1,
+          position: 'right top'
+        })
         console.log(error.text);
       }
     );
+    setState({
+      from_name: "",
+      email: "",
+      mobile: "",
+      message: ""
+    })
+    setSelectedOption(null)
   };
 
   return ( 
@@ -46,11 +104,11 @@ function Contact() {
             <div className="first-column">
               <div>
                 {/* <label htmlFor="name">Full Name</label> */}
-                <input type="text" name="from_name" id="name" required placeholder="Full Name" />
+                <input type="text" name="from_name" id="name" required placeholder="Full Name" value={state.from_name} onChange={handleChange} />
               </div>
               <div>
                 {/* <label htmlFor="email">Email ID</label> */}
-                <input type="email" name="email" id="email" required  placeholder="Email ID"/>
+                <input type="email" name="email" id="email" required  placeholder="Email ID" value={state.email} onChange={handleChange}/>
               </div>
             </div>
               <div>
@@ -61,6 +119,7 @@ function Contact() {
                   id="mobile-number"
                   required
                   placeholder="Mobile Number"
+                  value={state.mobile} onChange={handleChange}
                 />
               </div>
               <div>
@@ -85,6 +144,7 @@ function Contact() {
               cols="30"
               rows="5"
               required
+              value={state.message} onChange={handleChange}
             ></textarea>
             <button className="submit-btn" type="submit">
               Submit
