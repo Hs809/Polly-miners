996fb336d497597f34219423046b251438e798aa
Hs809 <hiteshpal.8097@gmail.com>
2022-08-15 15:54:29 +0530

    updated customers

--- components/Customers.js
+++ components/Customers.js
@@ -41,8 +41,12 @@ function Customers() {
             <a>
               <div className="ccard cccard1">
                 <div className="cc-content">
-                  <h2>Ishan Zotac 3080 Mining Rig</h2>
-                  <p>Lorem to pur description of that fucked up person</p>
+                  <h2>Ishan Shaik Zotac 3080 Mining Rig </h2>
+                  <p>
+                    Our client Ishan Shaik has build a Mining Rig of 3 cards
+                    3080 Zotac Graphic Card with 300 MHS. Getting a profit of{" "}
+                    <bold>18K per month</bold>.
+                  </p>
                 </div>
               </div>
             </a>
@@ -57,8 +61,11 @@ function Customers() {
             <a>
               <div className="ccard cccard2">
                 <div className="cc-content">
-                  <h2>Rocshon 3060ti * 8 Mining Rig </h2>
-                  <p>Lorem to pur description of that fucked up person</p>
+                  <h2>Reckson Mixed 1660 Super and 3060ti Mining Rig </h2>
+                  <p>
+                    Our client Reckson build a 1660 super with 10 GPU&apos;s and
+                    3060i with 3 GGPU{" "}
+                  </p>
                 </div>
               </div>
             </a>
@@ -73,18 +80,60 @@ function Customers() {
             <a>
               <div className="ccard cccard3">
                 <div className="cc-content">
-                  <h2>Rahul Gaming PC</h2>
-                  <p>Lorem to pur description of that fucked up person</p>
+                  <h2>Rahul Karmokar Gaming PC</h2>
+                  <p>
+                    Our Client Rahul build a Good Gaming PC with 1660 6gb GPU
+                    with Intel I7-12700 Processor and 16GB Ram and a Beautiful
+                    cabinet
+                  </p>
                 </div>
               </div>
             </a>
           </Link>
         </SwiperSlide>
-        ...
+        <SwiperSlide>
+          <Link
+            href="https://www.youtube.com/watch?v=ZDedq0Qvp2s&t=37s"
+            passHref={true}
+            target="_blank"
+          >
+            <a>
+              <div className="ccard cccard3">
+                <div className="cc-content">
+                  <h2>Rahul Karmokar Gaming PC</h2>
+                  <p>
+                    Our Client Rahul build a Good Gaming PC with 1660 6gb GPU
+                    with Intel I7-12700 Processor and 16GB Ram and a Beautiful
+                    cabinet.
+                  </p>
+                </div>
+              </div>
+            </a>
+          </Link>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Link
+            href="https://www.youtube.com/watch?v=jWz8oaKLEKs"
+            passHref={true}
+            target="_blank"
+          >
+            <a>
+              <div className="ccard cccard4">
+                <div className="cc-content">
+                  <h2>Jagir Dave 3060ti Mining Rig</h2>
+                  <p>
+                    Our client Jagir has build a Mining Rig of 8 cards 3060ti
+                    Graphic Card with total 516 MHS. We have Provided him with
+                    One year service on GPU&apos;s.
+                  </p>
+                </div>    
+              </div>
+            </a>
+          </Link>
+        </SwiperSlide>
       </Swiper>
     </div>
   );
 }
 
 export default Customers;
-
